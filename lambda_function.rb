5bbb769b69f18a8cd2c6ae6c64051835a9085734
keisukesaito7 <keisukesaito7@gmail.com>
2022-07-23 10:23:09 +0900

    :recycle: USD → JPY

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,5 +1,9 @@
 require 'slack-notifier'
 require 'aws-sdk-costexplorer'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'pry'
 
 def lambda_handler(event:, context:)
   message = fetch_cost
@@ -26,24 +30,41 @@ def pretty_response(message)
   end_date = message.dig(:results_by_time, 0, :time_period, :end)
 
   sum = 0
+  rate = exchange_rate_from_dollar_to_yen
   cost_groups = message.dig(:results_by_time, 0, :groups).map do |group|
     key = group.dig(:keys, 0)
-    amount = group.dig(:metrics, "AmortizedCost", :amount).to_f.round(2)
-    unit = group.dig(:metrics, "AmortizedCost", :unit)
+    amount = exchange_from_dollar_to_yen({
+      amount: group.dig(:metrics, "AmortizedCost", :amount).to_f,
+      rate: rate
+    })
 
     sum += amount
 
-    "#{key}: #{amount} #{unit}"
+    "#{key}: #{round(amount)}円"
   end
 
   <<~"EOS"
     ===========================
     #{start_date} - #{end_date}
     ---------------------------
-    sum: #{sum} USD
+    sum: #{round(sum)}円
     #{cost_groups.join("\n")}
     ===========================
   EOS
+end
+
+def exchange_rate_from_dollar_to_yen
+  uri = URI.parse("https://openexchangerates.org/api/latest.json?app_id=#{ENV['OPENEXCHANGERATES_API_ID']}")
+  res = Net::HTTP.get(uri)
+  JSON.parse(res)['rates']['JPY']
+end
+
+def exchange_from_dollar_to_yen(amount:, rate:)
+  amount * rate
+end
+
+def round(amount)
+  amount.round
 end
 
 def notify_slack(result)
