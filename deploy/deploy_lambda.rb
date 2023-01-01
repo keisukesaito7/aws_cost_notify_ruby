b21b618cec1f67592a6624a1e9f756b14bfa7e2c
keisukesaito7 <keisukesaito7@gmail.com>
2023-01-01 13:03:14 +0900

    :recycle: refactor, use github oidc

--- deploy/deploy_lambda.rb
+++ deploy/deploy_lambda.rb
@@ -2,15 +2,13 @@
 
 require 'aws-sdk-lambda'
 
-client = Aws::Lambda::Client.new({
-                                   region: ENV['REGION'],
-                                   access_key_id: ENV['ACCESS_KEY_ID'],
-                                   secret_access_key: ENV['SECRET_ACCESS_KEY']
-                                 })
+client = Aws::Lambda::Client.new({ region: ENV['REGION'] })
 
 zip_file = File.open 'costNotifierRuby.zip', 'r'
 
-client.update_function_code({
-                              function_name: 'costNotifierRuby',
-                              zip_file: zip_file
-                            })
+client.update_function_code(
+  {
+    function_name: 'costNotifierRuby',
+    zip_file: zip_file
+  }
+)
